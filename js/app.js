47281cf0767fe953e265a7802eb0cec8200084f2
Pep Ferin <pferin@venca.es>
2022-11-25 19:59:25 +0100

    Auth login & logout

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// MENU CLASS ACTIVE
+//MENU CLASS ACTIVE
 const mainMenu = document.getElementById('mainNavList');
 const listItems = mainMenu.getElementsByClassName("list-inline-item");
 
@@ -9,6 +9,24 @@ for (let i = 0; i < listItems.length; i++) {
         this.className += " active";
     });
 }
+
+// TOGGLE VIEW PASSWORD
+const loginPassword = document.querySelector('#loginPassword');
+const iconPw = document.querySelector('.input-group-text > i')
+
+ function togglePw() {
+    if (loginPassword.type === 'password') {
+        loginPassword.type = 'text'
+        iconPw.classList.toggle('bi-eye-slash')
+    } else {
+        loginPassword.type = 'password'
+        iconPw.classList.toggle('bi-eye-slash')
+    }
+}
+
+iconPw.addEventListener('click', togglePw);
+
+
 
 // DATE
 let date =  new Date().getFullYear();
--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, signOut  } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +17,47 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// AUTHENTICATION
+const auth = getAuth(app);
+
+const login = document.querySelector('#login');
+const logout = document.querySelector('#logout');
+const loginForm = document.querySelector('#loginForm');
+const mainRecipes = document.querySelector('#mainRecipes');
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const loginMail = document.querySelector('#loginMail').value; 
+    const loginPassword = document.querySelector('#loginPassword').value;
+
+    signInWithEmailAndPassword(auth, loginMail, loginPassword)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        login.classList.add('d-none');
+        mainRecipes.classList.add('d-block');
+        console.log('signed')
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode + errorMessage)
+    });
+});
+
+logout.addEventListener('click', () => {
+    signOut(auth).then(() => {
+        console.log('logout success')
+        login.classList.remove('d-none');
+        mainRecipes.classList.remove('d-block');
+        loginForm.reset();     
+    })
+    .catch((error) => {
+        console.log('An error happened');
+    });
+});
+
+
+
